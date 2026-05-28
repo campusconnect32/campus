69a0da193680e219647f468dd7cc5bc5de5fa1fd
jwconnect <mpilongwenya085@gmail.com>
2026-05-28 13:49:45 +0200

    Fix CORS origin list

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_origins=["*"],
+    allow_origins=["*"]
 )
 
 api_router = APIRouter(prefix="/api")
